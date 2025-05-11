f78d4db057e89c4dbff20654df403d43bd7ee9d2
Joel Southall <vextechmage@gmail.com>
2025-05-11 16:07:20 -0700

    Add Shared Resources page and refine Events layout

---
+++ DotnetMeetup/DotnetMeetup.Shared/Models/EventEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetMeetup.Shared.Models
+{
+    internal class EventEntry
+    {
+    }
+}
