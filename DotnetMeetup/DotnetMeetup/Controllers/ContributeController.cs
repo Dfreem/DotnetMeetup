b6373b71143acc53ad4ca2e72ed2d687006abdb1
Joel Southall <vextechmage@gmail.com>
2025-05-13 02:38:42 -0700

    Add Contribute form

---
+++ DotnetMeetup/DotnetMeetup/Controllers/ContributeController.cs
@@ -0,0 +1,24 @@
+﻿using DotnetMeetup.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetMeetup.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ContributeController : ControllerBase
+{
+    private readonly ILogger<ContributeController> _logger;
+
+    public ContributeController(ILogger<ContributeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public IActionResult SubmitTalk([FromBody] ContributeTalkDto talk)
+    {
+        _logger.LogInformation("Received talk proposal: {Title} by {Name}", talk.TalkTitle, talk.Name);
+        // TODO: Save to database or email to organizer
+        return Ok();
+    }
+}
